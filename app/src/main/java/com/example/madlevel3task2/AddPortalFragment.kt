bf5d877fb757a67a1f10df14f5884250eb7566e9
4libutt <ali@vergelijkgroep.nl>
2020-10-09 14:11:01 +0200

    added portal and portal adapter class. added portal_item layout. fixed navigation names and fixed addportal layout filename. removed placeholder button functions

--- app/src/main/java/com/example/madlevel3task2/AddPortalFragment.kt
+++ app/src/main/java/com/example/madlevel3task2/AddPortalFragment.kt
@@ -18,14 +18,11 @@ class AddPortalFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_fragment, container, false)
+        return inflater.inflate(R.layout.fragment_add_portal, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_second).setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
-        }
     }
 }
