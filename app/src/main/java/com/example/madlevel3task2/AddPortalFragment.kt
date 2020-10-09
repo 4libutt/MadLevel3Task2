76fa3e001e5a51e4d26e36ddb3298990c7a505cc
4libutt <ali@vergelijkgroep.nl>
2020-10-09 14:49:54 +0200

    updated build.gradle file. added functions to add portalItems to recyclerview. Made Portal Class parceable. added navcontroller and fabtoggler. fixed layoutmanager

--- app/src/main/java/com/example/madlevel3task2/AddPortalFragment.kt
+++ app/src/main/java/com/example/madlevel3task2/AddPortalFragment.kt
@@ -6,7 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.Toast
+import androidx.core.os.bundleOf
+import androidx.fragment.app.setFragmentResult
 import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_add_portal.*
+
+
+const val PORTAL = "portal"
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -24,5 +31,30 @@ class AddPortalFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        btnAddPortal.setOnClickListener {
+            onAddPortal()
+        }
+    }
+
+    private fun onAddPortal() {
+        val portalTitle = etTitle.text.toString()
+        val portalUrl = etUrl.text.toString()
+
+        if (portalTitle.isNotBlank() and portalUrl.isNotBlank()) {
+            setFragmentResult(
+                PORTAL,
+                bundleOf(
+                    PORTAL to Portal(portalTitle, portalUrl)
+                )
+            )
+
+            findNavController().popBackStack()
+        } else {
+            Toast.makeText(
+                activity,
+                R.string.invalid_portal,
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 }
--- app/src/main/java/com/example/madlevel3task2/PortalFragment.kt
+++ app/src/main/java/com/example/madlevel3task2/PortalFragment.kt
@@ -1,17 +1,25 @@
 package com.example.madlevel3task2
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.fragment.app.setFragmentResultListener
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.fragment_portal.*
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class PortalFragment : Fragment() {
+
+    private val portals = arrayListOf<Portal>()
+    private val portalAdapter = PortalAdapter(portals)
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -24,5 +32,16 @@ class PortalFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        rvPortalCards.layoutManager = GridLayoutManager(context, 2, RecyclerView.VERTICAL, false)
+        rvPortalCards.adapter = portalAdapter
+    }
+
+
+    private fun observeAddReminderResult() {
+        setFragmentResultListener(PORTAL) { _, bundle ->
+            bundle.getParcelable<Portal>(PORTAL)?.let {
+                portals.add(it)
+            } ?: Log.e("PortalCard", "No PortalCard received!")
+        }
     }
 }
